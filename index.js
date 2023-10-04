848d887cafa5c5bc14b6d2ca1ba4f5dd30f648f9
JosephKagwi <josephkagwi28@gmail.com>
2023-10-05 00:21:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function isPalindrome(word) {
+
   // Write your algorithm here
+  // Helper function to reverse a string
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
+
+// Main function to check if a word is a palindrome
+function isPalindrome(word) {
+  // Reverse the input string
+  const reversedWord = reverseString(word);
+
+  // Compare the reversed string to the input
+  return word === reversedWord;
+}
+  
+
 
 /* 
   Add your pseudocode here
@@ -13,6 +27,7 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
